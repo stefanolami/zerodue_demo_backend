6f82aa56d13e7841f9a092b0f3464b193e3fd343
stefanolami <stefanolami90@gmail.com>
2022-09-15 16:30:37 +0200

    Update database.js

--- database.js
+++ database.js
@@ -2,10 +2,10 @@ const mysql = require('mysql2');
 
 const db = mysql.createPool({
     connectionLimit : 100,
-    host: 'localhost',
-    user: 'root',
-    password: 'shaman90',
-    database: 'zerodue_final'
+    host: 'eu-cdbr-west-03.cleardb.net',
+    user: 'b04ddce483bfc3',
+    password: 'b41252bd',
+    database: 'heroku_a600ef4280fca9f'
 })
 
 exports.db = db;
